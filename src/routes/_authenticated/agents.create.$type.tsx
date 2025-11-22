489f4c27b305944421d47b0b53cca34c6065cdd3
lorencianodramayo <lorencianodramayo@gmail.com>
2025-11-22 13:21:07 +0800

    refactor(routes): remove deprecated agent creation route - Remove agents.create..tsx in favor of agents.create.tsx

--- src/routes/_authenticated/agents.create.$type.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import AgentBuilderPage from "@/pages/AgentBuilderPage";
-
-export const Route = createFileRoute("/_authenticated/agents/create/$type")({
-  component: AgentBuilderPage,
-});
